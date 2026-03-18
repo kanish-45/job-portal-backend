11ba8a41bebf81abb3b1575b17bc30cba0ee666d
kanish-45 <154958446+kanish-45@users.noreply.github.com>
2026-03-18 12:20:59 +0530

    jobRoutes.js

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -3,11 +3,6 @@ const router = express.Router();
 
 const { createJob, getJobs } = require("../controllers/jobController");
 
-// 🔥 ADD THIS DEBUG ROUTE
-router.get("/test", (req, res) => {
-  res.send("Job route working ✅");
-});
-
 router.post("/", createJob);
 router.get("/", getJobs);
 
